c7340ecf42a20f254131573aed70a952841f4fe9
seitamuro <seitamuro0210@gmail.com>
2023-03-01 17:40:49 +0900

    遷移先のページのUIを整えた

--- lib/next_page.dart
+++ lib/next_page.dart
@@ -11,6 +11,39 @@ class _NextPageState extends State<NextPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Container(
+                  width: 80,
+                  child: Text("ID")
+                ),
+                Container(
+                  width: 20,
+                  child: Text(":")
+                )
+              ],
+            ),
+            Padding(padding: EdgeInsets.all(10)),
+            Row(
+              children: [
+                Container(
+                  width: 80,
+                  child: Text("Password")
+                ),
+                Container(
+                  width: 20,
+                  child: Text(":"),
+                )
+              ],
+            ),
+          ],
+        ),
+      )
     );
   }
 }
